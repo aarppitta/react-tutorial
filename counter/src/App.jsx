1c2fd7efca2d5600a687b588597e73cbb552dde3
arpitaquarks <arpita@quarkssystems.com>
2026-01-28 18:36:16 +0530

    vite-react-js

---
+++ counter/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1>Counter App</h1>
+      <p>Counter value is:{}</p>
+
+      <button>Decrement</button>
+     <br />
+      <button>Increment</button>
+    </>
+  )
+}
+
+export default App
